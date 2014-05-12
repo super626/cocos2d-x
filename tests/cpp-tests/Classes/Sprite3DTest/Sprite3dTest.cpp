aec5d272c36818195921d28213461b17d68a1845
Huabing.Xu <dabingnn@gmail.com>
2014-05-12 15:42:35 -0700

    add sprite3D test cases

--- tests/cpp-tests/Classes/Sprite3DTest/Sprite3dTest.cpp
+++ tests/cpp-tests/Classes/Sprite3DTest/Sprite3dTest.cpp
@@ -6,4 +6,13 @@ void Sprite3dTestScene::runThisTest()
     auto layer = LayerColor::create(Color4B(80,95,203,255));
     addChild(layer);
     Director::getInstance()->replaceScene(this);
+    auto model = Sprite3D::create("sprite3d/boss.obj","sprite3d/boss.png");
+    model->setPosition(Vector2(200,200));
+    model->setScale(20);
+//    model->runAction(Spawn::create(
+//                                   RotateBy::create(1, 2),
+//                                   RotateBy,
+//                                   NULL));
+    model->runAction(RotateBy::create(2, 40));
+    layer->addChild(model);
 }
